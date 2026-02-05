3c3c90949fb709e158761c44bb1def7b17fb97da
Geumdalla <geumdalla@gmail.com>
2026-02-06 03:45:29 +0900

    NavInit

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 import { cn } from "@/lib/styleUtils";
 import NavItem from "@/ui/nav/NavItem";
+import Content from "@/components/nav/article/Content";
 
 const mobileStyles = "w-full h-14 bg-white border-t border-gray-200 flex items-center justify-around px-2 pb-safe";
 
@@ -21,25 +22,38 @@ const desktopStyles = `
   `;
 
 const tempNavItems = [
-  { icon: <Home />, label: "홈" },
-  { icon: <FolderTree />, label: "글" },
-  { icon: <Search />, label: "검색" },
-  { icon: <PlusSquare />, label: "알림" },
-  { icon: <User />, label: "내 정보" },
+  { Icon: Home, label: "홈" },
+  { Icon: FolderTree, label: "글", Content: <Content /> },
+  { Icon: Search, label: "검색" },
+  { Icon: PlusSquare, label: "알림" },
+  { Icon: User, label: "내 정보" },
 ];
 
 //TODO: NavItem은 주입받아서 렌더링
 export default function ResponsiveNav() {
-  const [ActiveItem, setActiveItem] = useState("home");
+  const [ActiveItem, setActiveItem] = useState<string | null>("home");
+
+  const activeHandler = (label: string) => {
+    if (ActiveItem === label) {
+      setActiveItem(null);
+    } else {
+      setActiveItem(label);
+    }
+  };
 
   return (
     <>
       {" "}
       <div className="fixed bottom-0 left-0 right-0 z-50 flex justify-center pointer-events-none">
         <nav className={cn("pointer-events-auto transition-all duration-300 ease-in-out", mobileStyles, desktopStyles)}>
-          {tempNavItems.map(({ icon, label }) => (
-            <span key={label} onClick={() => setActiveItem(label)}>
-              <NavItem icon={icon} label={label} isActive={ActiveItem === label} />
+          {tempNavItems.map(({ Icon, label, Content }) => (
+            <span
+              key={label}
+              onClick={() => {
+                activeHandler(label);
+              }}
+            >
+              <NavItem Icon={Icon} label={label} isActive={ActiveItem === label} Content={Content} />
             </span>
           ))}
         </nav>
--- src/ui/nav/NavItem.tsx
+++ src/ui/nav/NavItem.tsx
@@ -1,38 +1,50 @@
-import Link from "next/link";
 import clsx from "clsx";
+import { LucideIcon } from "lucide-react";
 
 interface NavItemProps {
-  icon: React.ReactNode;
+  Icon: LucideIcon;
   label: string;
   isActive: boolean;
+  Content: React.ReactNode;
   href?: string;
   onClick?: () => void;
 }
 
-export default function NavItem({ icon, label, isActive, href, onClick }: NavItemProps) {
-  const className = "group relative flex flex-col items-center justify-center p-2";
-  const iconStyles =
-    "w-6 h-6 text-gray-800 transition-transform group-hover:scale-110 md:w-8 md:h-8 md:text-black flex items-center justify-center";
-  const activeIconStyles = "text-red-500 scale-200 group-hover:scale-200";
+export default function NavItem({ Icon, label, isActive, Content }: NavItemProps) {
+  const iconStyles = `
+  w-6 h-6 text-gray-800 transition-transform duration-200 ease-out group-hover:scale-140
+  md:w-8 md:h-8 md:text-black flex items-center justify-center
+  `;
+  const activedIconStyles = "scale-200 group-hover:scale-200";
 
   const content = (
     <>
-      <div className={clsx(iconStyles, isActive && activeIconStyles)}>{icon}</div>
+      <div className={clsx(iconStyles, isActive && activedIconStyles)}>
+        <Icon color={isActive ? "red" : "black"} />
+      </div>
       <span className="sr-only">{label}</span>
     </>
   );
 
-  if (href) {
-    return (
-      <Link href={href} className={className}>
-        {content}
-      </Link>
-    );
-  }
-
   return (
-    <button className={className} onClick={onClick}>
-      {content}
-    </button>
+    <div className="relative">
+      {Content && (
+        <div
+          className={`
+            absolute bottom-15 left-1/2 -translate-x-1/2 w-max
+            bg-blue-500 rounded px-2 py-1
+            transition-transform duration-200 ease-out origin-bottom
+            ${
+              isActive
+                ? "opacity-100 visible scale-100 translate-y-0" // 활성: 원래 크기, 원래 위치
+                : "opacity-0 invisible scale-0 translate-y-4 pointer-events-none" // 비활성: 작아지고, 약간 아래로, 클릭 방지
+            }
+          `}
+        >
+          {Content}
+        </div>
+      )}
+      <button className="group relative flex flex-col items-center justify-center p-2">{content}</button>
+    </div>
   );
 }
