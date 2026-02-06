ba14dcf70bc1cfdc0bb92567a96371107d7ea5d0
Geumdalla <geumdalla@gmail.com>
2026-02-07 04:04:38 +0900

    refactor: reorganize folder structure and clean up code

--- src/components/nav/NavContainer.tsx
+++ src/components/nav/NavContainer.tsx
@@ -1,8 +1,8 @@
 import NavigationProvider from "@/components/nav/NavigationProvider";
 
 // 이곳에서 버튼들을 import 해와서 직접 뿌려주기
-import Home from "@/components/nav/NavItems/Home";
-import Article from "@/components/nav/NavItems/Article/Container";
+import Home from "@/components/nav/navItems/Home";
+import Article from "@/components/nav/navItems/Article/Container";
 
 export default function NavContainer() {
   return (
